40e2d3b21db8c1f7ea3293ad91cae160d10f2827
Caleb Tafawa-balewa <tafawabalewacaleb@gmail.com>
2022-07-09 22:15:24 +0100

    Initial Commit

--- app/src/main/java/co/danjuma/snipicalculator/CalcViewModel.kt
+++ app/src/main/java/co/danjuma/snipicalculator/CalcViewModel.kt
@@ -5,50 +5,110 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
-class CalcViewModel: ViewModel() {
+class CalcViewModel : ViewModel() {
 
-    var state by mutableStateOf(CalcState())
+    private var state by mutableStateOf(CalcState())
+        private set
 
-    private set
 
-
-    fun onAction(action: CalcAction){
-        when(action){
+    fun onAction(action: CalcAction) {
+        when (action) {
 
             is CalcAction.Number -> enterNumber(action.number)
             is CalcAction.Decimal -> enterDecimal()
             is CalcAction.Clear -> state = CalcState()
             is CalcAction.Operation -> enterOperation(action.operation)
             is CalcAction.Calculate -> performCalculate()
-            is CalcAction.Delete -> PerformDeletion()
+            is CalcAction.Delete -> performDeletion()
 
         }
     }
 
-    class PerformDeletion {
+    private fun performDeletion() {
+        when {
+            state.number2.isNotBlank() -> state = state.copy(
+                number2 = state.number2.dropLast(1)
+            )
+
+            state.operation != null -> state = state.copy(
+                operation = null
+            )
+
+            state.number1.isNotBlank() -> state = state.copy(
+                number2 = state.number1.dropLast(1)
+            )
+        }
 
     }
 
     private fun performCalculate() {
-        TODO("Not yet implemented")
+        val number1 = state.number1.toDoubleOrNull()
+        val number2 = state.number2.toDoubleOrNull()
+        if (number1 != null && number2 !== null) {
+            val result = when (state.operation) {
+                is CalcOperation.Add -> number1 + number2
+                is CalcOperation.Subtract -> number1 - number2
+                is CalcOperation.Multiply -> number1 * number2
+                is CalcOperation.Divide -> number1 / number2
+                null -> return
+            }
+        }
     }
 
     private fun enterOperation(operation: CalcOperation) {
 
-        if(state.number1.isNotBlank()){
+        if (state.number1.isNotBlank()) {
             state = state.copy(operation = operation)
         }
-
-
 
     }
 
     private fun enterDecimal() {
-        TODO("Not yet implemented")
+        if (
+               state.operation == null
+            && !state.number1.contains(".")
+            && state.number1.isNotBlank()
+        ) {
+            state = state.copy(
+                number1 = state.number1 + "."
+            )
+            return
+        }
+
+        if (!state.number2.contains(".") && state.number2.isNotBlank()) {
+            state = state.copy(
+                number1 = state.number2 + "."
+            )
+
+        }
     }
 
     private fun enterNumber(number: Int) {
 
+        if (state.operation == null) {
+            if (state.number1.length >= MAX_NUM_LENGTH) {
+                return
+            }
+
+            state = state.copy(
+                number2 = state.number2 + number
+            )
+            return
+        }
+
+        if (state.number2.length >= MAX_NUM_LENGTH) {
+            return
+        }
+
+        state = state.copy(
+            number2 = state.number2 + number
+        )
+
+
+    }
+
+    companion object {
+        private const val MAX_NUM_LENGTH = 8
     }
 
 }
